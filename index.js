f8468e94d2f6a59bcf84b90f27dbea5bed722fee
Quinton Fults <quinton.fults.work@gmail.com>
2021-03-30 13:22:05 -0700

    code

---
+++ index.js
@@ -0,0 +1,22 @@
+// const db = require('mongojs')('favoritesdb')
+// const ObjectId = require('mongojs').ObjectId
+
+// db.movies.find({ _id: ObjectId('606384bb08d85d3e32dd63ea') }, (err, movies) => {
+//   if (err) { console.log(err) }
+//   console.log(movies)
+// })
+
+// db.movies.insert({ name: 'The Room' }, (err, movie) => {
+//   if (err) { console.log(err) }
+//   console.log(movie)
+// })
+
+// db.movies.update({ _id: ObjectId('606384bb08d85d3e32dd63ea') }, { $set: { name: 'GOODFELLAS' } }, (err, fields) => {
+//   if (err) { console.log(err) }
+//   console.log(fields)
+// })
+
+// db.movies.remove({ _id: ObjectId('606384bb08d85d3e32dd63ea') }, (err, fields) => {
+//   if (err) { console.log(err) }
+//   console.log(fields)
+// })
